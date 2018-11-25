483c43c19744905bc3a0e8762dbadaab931ea8ee
J/A <archer884@gmail.com>
2018-11-24 23:38:19 -0600

    First cut

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use hashbrown::HashSet;
+use std::io::{self, Read, Write};
+
+fn main() -> io::Result<()> {
+    let handle = io::stdout();
+
+    let mut buf = String::new();
+    let mut set = HashSet::new();
+    let mut out = handle.lock();
+
+    io::stdin().read_to_string(&mut buf)?;
+
+    for line in buf.lines() {
+        let line = line.trim();
+
+        if set.insert(line) {
+            write!(out, "{}", line)?;
+        }
+    }
+
+    Ok(())
+}
